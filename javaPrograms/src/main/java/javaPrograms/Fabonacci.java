1232303e461a3931f806429309fbeb6699bd9d9d
lion <lion@parrot>
2019-04-30 16:15:02 +0530

    this is my first commit

---
+++ javaPrograms/src/main/java/javaPrograms/Fabonacci.java
@@ -0,0 +1,43 @@
+package javaPrograms;
+
+public class Fabonacci {
+
+	public static void main(String[] args) {
+		System.out.println(fibo(12)+"");
+		fibonacci(13);
+	}
+
+	
+	public static int fibo(int x) {
+		int fabonacci=0;
+		if(x==1||x==2)
+			return 1;
+		int fab1=1,fab2=1;
+		for(int y=3;y<=x;y++) {
+			fabonacci= fab1+fab2;
+			fab1=fab2;
+			fab2=fabonacci;
+		}
+		
+		return fabonacci;
+	}
+	
+	  //jhhlkhdrder
+	
+
+		public static int fibonacci(int number){
+      if(number == 1 || number == 2){
+    	  return 1; 
+    	}
+      int z= fibonacci(number-1) + fibonacci(number -2);
+      System.out.println(z);
+      	return z; 
+      			
+      }
+
+		
+	
+	
+	
+	
+}
